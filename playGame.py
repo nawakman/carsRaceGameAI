b93bac741a673cc90b352a9e35f18d9b8168375a
Nawakman <themistobreizh@gmail.com>
2025-03-16 19:45:29 +0100

    we can now replay AI training or play against AI(loaded from text file) in the python game

--- playGame.py
+++ playGame.py
@@ -22,7 +22,6 @@ class carsRace:
         self.wallColor=(0,0,0)
         self.playerMoveColor=(0,0,220)
         self.clock=pygame.time.Clock()
-        self.playerTurn=True
 
         self.maxSpeed=10
         self.playerSpeed=0
@@ -37,45 +36,88 @@ class carsRace:
         
         self.LoadMap(mapPictureFile,mapResolution)#this function will set self.windowResolution used by next function
         self.screen=pygame.display.set_mode(self.windowResolution)
+   
+    def Play(self,mode="alone",AIFile=""):#3 modes, "alone"/"vsAI"/"AI"
+        self.mode=mode
+        self.currentTurn=1#first turn is 1 not 0
+        match mode:
+            case "alone":
+                self.PlayAlone()
+            case "vsAI":
+                self.LoadAI(AIFile)
+                self.PlayAgainstAI()
+            case "AI":
+                self.LoadAI(AIFile)
+                self.VisualizeGeneration()
 
-        #GAME LOOP
+    def PlayAlone(self):
         while True:
             #EVENTS
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     quit()
             #INPUTS
-            self.mousePosition=pygame.mouse.get_pos()
-            avoidFloorSnapping=self.tileSize/2
-            self.mousePosition=((self.mousePosition[0]+avoidFloorSnapping)//self.tileSize,(self.mousePosition[1]+avoidFloorSnapping)//self.tileSize)#convert mouse position to the nearest grid position instead of the smaller grid position
-            self.mouseState=pygame.mouse.get_pressed()
+            self.UpdatePlayerInputs()
+            #GAMEPLAY
+            if self.mouseState[0]:#left click
+                self.PlayerMove()
+                if self.IsFinishLine(self.playerMoves[-1]):
+                    print("player won")
+                pygame.time.delay(150)
+            #RENDER
+            self.Render()       
+
+    def PlayAgainstAI(self):
+        self.playerTurn=True
+
+        while True:
+            #EVENTS
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+            #INPUTS
+            self.UpdatePlayerInputs()
+
             #GAMEPLAY
             if self.playerTurn:
                 if self.mouseState[0]:#left click
                     self.PlayerMove()
-                    self.playerTurn=False
+
                     if self.IsFinishLine(self.playerMoves[-1]):
                         print("player won")
-            else:
-                self.AskAIMove()
-                self.playerTurn=True
-                if self.IsFinishLine(self.AIMoves[-1]):
-                        print("AI won")
-            #RENDER
-            self.DrawMap()
-            self.DrawPoints()
-            self.DrawMoves(self.playerMoves,self.playerMoveColor)
-            self.DrawNextPlayerMove()
-            self.DrawMoves(self.AIMoves,(100,100,0))
 
-            pygame.display.update()
-            self.clock.tick(60)#60fps
+                    pygame.time.delay(150)
+                    self.playerTurn=False
+            else:
+                if len(self.AIMoves[0])>self.currentTurn:#if AI still have moves
+                    if self.IsFinishLine(self.AIMoves[0][self.currentTurn]):
+                        print("AI won")
+                else:
+                    print("AI has no more moves, cannot reach finish line") 
+                self.playerTurn=True
+                self.currentTurn+=1
+            #RENDER
+            self.Render()
+
+    def VisualizeGeneration(self):#it only display AI Moves, they can be wrong if the data in the file is wrong 
+        while True:
+            #EVENTS
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+            #GAMEPLAY
+            pygame.time.delay(1000)#display a new move each x milliseconds
+            self.currentTurn+=1
+            print(self.currentTurn)
+            #RENDER
+            self.Render()
 
     def LoadMap(self,mapPictureFile,mapResolution):
         self.mapMaker=pictureToCircuit(mapPictureFile,mapResolution)
 
-        tileFilePath="circuits/"+mapPictureFile.split(".")[0]+".txt"
-        pointsFilePath="circuits/"+mapPictureFile.split(".")[0]+"Points.txt"
+        self.mapName=mapPictureFile.split(".")[0]
+        tileFilePath="circuits/"+self.mapName+".txt"
+        pointsFilePath="circuits/"+self.mapName+"Points.txt"
         if not os.path.isfile(tileFilePath):#if map text files does not already exists then generate
             self.mapMaker.CreateCircuitFromPicture()#generates needed text files for next functions
         self.ReadTileMapFromFile(tileFilePath)
@@ -86,6 +128,34 @@ class carsRace:
             self.ReadTileMapFromFile(tileFilePath)#refresh map since it has been regenerated
             self.ReadPointsFromFile(pointsFilePath)
 
+    def LoadAI(self,AIFile):#mapName in first line #nb individuals in second line
+        self.AIMoves=[]#will look like [[ai1][ai2][ai3],...] and each "aiX" will look like [[x1,y1],[x2,y2],[x3,y3],...]
+
+        filePath="AI/"+AIFile
+        if not os.path.isfile(filePath):#if map text files does not already exists then generate
+            print("AI file"+filePath+"does not exist")
+            quit()
+        with open(filePath) as file:
+            mapNameFromFile=file.readline().rstrip("\n")#first line contains the name of the map the AI was trained on
+            if not self.mapName==mapNameFromFile:#maps need to match
+                print("This AI was trained on: "+mapNameFromFile+" but you loaded: "+self.mapName)
+                quit()
+
+            nbIndividuals=int(file.readline())
+            for i in range(nbIndividuals):
+                line=file.readline()
+                individualMoves=[]#temporary container to store all moves of an individual
+                coordinates=line.split(";")
+
+                for strCoord in coordinates:
+                    coord=[int(x) for x in strCoord.split(",")]
+                    coord[0],coord[1]=coord[1],coord[0]#in the c program coord are (line, column), in this script it is (column, line)
+                    coord=pygame.math.Vector2(coord)#convert to vector2
+                    individualMoves.append(coord)
+
+                self.AIMoves.append(individualMoves)
+            print(self.AIMoves)
+        
     def ReadTileMapFromFile(self,filePath):
         with open(filePath) as file:
             self.tileMatrixSize=[int(x) for x in file.readline().split(";")]#parse first line to int
@@ -116,9 +186,28 @@ class carsRace:
                     if line[x]=="s":
                         self.start=pygame.math.Vector2(x,y)
                         self.playerMoves=[self.start]
-                        self.AIMoves=[self.start]
+                        #self.AIMoves=[self.start] #when we'll read AI moves from file it will already start at start
                     elif line[x]=="e":
                         self.end=pygame.math.Vector2(x,y)
+
+    def Render(self):
+        self.DrawMap()
+        self.DrawPoints()
+        self.DrawCrashes()
+        match self.mode:
+            case "alone":
+                self.DrawMoves(self.playerMoves,self.playerMoveColor)
+                self.DrawNextPlayerMove()
+            case "vsAI":
+                self.DrawMoves(self.playerMoves,self.playerMoveColor)
+                self.DrawNextPlayerMove()
+                self.DrawMoves(self.AIMoves[0][:self.currentTurn],(100,100,0))#AI are stored from best to worst #only display moves up to the current turn
+            case "AI":
+                for ai in self.AIMoves:
+                    self.DrawMoves(ai[:min(self.currentTurn,len(ai))],(100,100,0))#if an ai finished faster it will have less moves #only draw up to current turn
+
+        pygame.display.update()
+        self.clock.tick(60)#60fps
 
     def DrawMap(self):
         self.screen.fill(self.backgroundColor)
@@ -165,61 +254,27 @@ class carsRace:
                     pygame.draw.circle(self.screen,startPointColor,(x*self.tileSize,y*self.tileSize),8)
                 elif self.pointMatrix[x][y]=="e":
                     pygame.draw.circle(self.screen,endPointColor,(x*self.tileSize,y*self.tileSize),8)
-    
-    def PlayerMove(self):#first get the actual move, then apply it
-        position,speed,direction=self.PossiblePlayerMove()
-        self.MovePlayer(speed,direction)
-
-    def MovePlayer(self,newSpeed,newDirection):#scan each tile from last position to final position to check if a crash happened
-        scannedTile=self.playerMoves[-1].copy()#we don't want to change the player move from the array
-        
-        for i in range(newSpeed):
-            if self.pointMatrix[int((scannedTile+newDirection)[0])][int((scannedTile+newDirection)[1])]=="n":#if next move will crash in wall then put player just before the wall
-                isMoveDiagonal=newDirection.angle_to(pygame.math.Vector2(1,0))%90!=0#if moving diagonally
-                crashPosition=scannedTile+newDirection*(0.5 if isMoveDiagonal and self.IsDiagonalTile(scannedTile+newDirection) else 1)#if we crash in a diagonal tile we need to put the point on the diagonal wall not on the tile behind
-                self.crashLocations.add((crashPosition[0],crashPosition[1]))#the set will remove duplicates by itself #convert to tuple because Vector2 i not hashable for set
-                
-                #if a crash is detected then the actual scannedTile is the position right before the wall
-                self.playerSpeed=0
-                self.playerDirection=pygame.math.Vector2(0,0)
-                if scannedTile!=self.playerMoves[-1]:#if two moves are in the same position game crahes (division by 0 I guess)
-                    self.playerMoves.append(scannedTile)
-                return
-            else:
-                scannedTile=scannedTile+newDirection#continue checking the path
-        
-        #if no crah were detected, scannedTile is now at the good position
-        self.playerSpeed=newSpeed
-        self.playerDirection=newDirection
-        self.playerMoves.append(scannedTile)
-    
-        
-    def IsFinishLine(self, position):
-        return self.pointMatrix[int(position[0])][int(position[1])]=="e"
-        
-
-    def IsDiagonalTile(self, position):
-        accessibleCount=0
-        for x in range(-1,2):
-            for y in range(-1,2):
-                accessibleCount+=1 if self.pointMatrix[int(position[0])+x][int(position[1])+y]=="y" else 0
-        return accessibleCount==1
-
-    def AskAIMove(self):
-        pygame.time.delay(150)#WIP, replace with actual AI program
 
     def DrawMoves(self,movesArray,color):
         for i in range(len(movesArray)-1):
             pygame.draw.line(self.screen,color,movesArray[i]*self.tileSize,movesArray[i+1]*self.tileSize,width=self.lineWidth)
             pygame.draw.circle(self.screen,color,movesArray[i+1]*self.tileSize,3)
+
+    def DrawCrashes(self):
         for crash in self.crashLocations:
             pygame.draw.circle(self.screen,(255,0,0),pygame.math.Vector2(crash)*self.tileSize,5)
-    
+
     def DrawNextPlayerMove(self):
         thisMove,speed,direction=self.PossiblePlayerMove()
         thisMove*=self.tileSize#scale from grid position to screen position
         lastMove=pygame.math.Vector2(self.playerMoves[-1])*self.tileSize
         pygame.draw.line(self.screen,self.playerMoveColor,lastMove,thisMove,width=self.lineWidth)
+
+    def UpdatePlayerInputs(self):
+        self.mousePosition=pygame.mouse.get_pos()
+        avoidFloorSnapping=self.tileSize/2
+        self.mousePosition=((self.mousePosition[0]+avoidFloorSnapping)//self.tileSize,(self.mousePosition[1]+avoidFloorSnapping)//self.tileSize)#convert mouse position to the nearest grid position instead of the smaller grid position
+        self.mouseState=pygame.mouse.get_pressed()
 
     def PossiblePlayerMove(self):
         mouseDirection=pygame.math.Vector2(self.mousePosition)-pygame.math.Vector2(self.playerMoves[-1])#vector from last player position to mouse
@@ -259,7 +314,45 @@ class carsRace:
             case 315:
                 result=pygame.math.Vector2(1,1)
         return result*speed+self.playerMoves[-1],speed,result#convert from local coordinates to world coordinates
+
+    def PlayerMove(self):#first get the actual move, then apply it
+        position,speed,direction=self.PossiblePlayerMove()
+        self.MovePlayer(speed,direction)
+
+    def MovePlayer(self,newSpeed,newDirection):#scan each tile from last position to final position to check if a crash happened
+        scannedTile=self.playerMoves[-1].copy()#we don't want to change the player move from the array
         
+        for i in range(newSpeed):
+            if self.pointMatrix[int((scannedTile+newDirection)[0])][int((scannedTile+newDirection)[1])]=="n":#if next move will crash in wall then put player just before the wall
+                isMoveDiagonal=newDirection.angle_to(pygame.math.Vector2(1,0))%90!=0#if moving diagonally
+                crashPosition=scannedTile+newDirection*(0.5 if isMoveDiagonal and self.IsDiagonalTile(scannedTile+newDirection) else 1)#if we crash in a diagonal tile we need to put the point on the diagonal wall not on the tile behind
+                self.crashLocations.add((crashPosition[0],crashPosition[1]))#the set will remove duplicates by itself #convert to tuple because Vector2 i not hashable for set
+                
+                #if a crash is detected then the actual scannedTile is the position right before the wall
+                self.playerSpeed=0
+                self.playerDirection=pygame.math.Vector2(0,0)
+                if scannedTile!=self.playerMoves[-1]:#if two moves are in the same position game crashes (division by 0 I guess)
+                    self.playerMoves.append(scannedTile)
+                return
+            else:
+                scannedTile=scannedTile+newDirection#continue checking the path
+        
+        #if no crah were detected, scannedTile is now at the good position
+        self.playerSpeed=newSpeed
+        self.playerDirection=newDirection
+        self.playerMoves.append(scannedTile)   
+        
+    def IsFinishLine(self, position):
+        return self.pointMatrix[int(position[0])][int(position[1])]=="e"
+        
+    def IsDiagonalTile(self, position):
+        accessibleCount=0
+        for x in range(-1,2):
+            for y in range(-1,2):
+                accessibleCount+=1 if self.pointMatrix[int(position[0])+x][int(position[1])+y]=="y" else 0
+        return accessibleCount==1
+
+
 def Clamp(x, minValue, maxValue):
     return max(min(x, maxValue), minValue)
 
@@ -346,3 +439,6 @@ def From360To180Range(angle):#from 0,360 to -180,180
     return angle if angle<=180 else angle-360
 
 game=carsRace("test1.png",10,0.75)
+game.Play("alone")
+#game.Play("vsAI","handMadeTest.txt")
+#game.Play("AI","handMadeTest.txt")
